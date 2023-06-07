93830c5d527be25dfb947ed8eec4fbed0c5cccec
YehielSiri <yehiels14@gmail.com>
2023-06-07 12:45:39 +0300

    opticalFlow - working

--- ex3_utils.py
+++ ex3_utils.py
@@ -20,12 +20,36 @@ from numpy.linalg import LinAlgError
 import matplotlib.pyplot as plt
 
 
+GRADIENT = np.array([[1, 0, -1]])
+
+
 def myID() -> np.int32:
     return 204155311
 
 # ---------------------------------------------------------------------------
 # ------------------------ Lucas Kanade optical flow ------------------------
 # ---------------------------------------------------------------------------
+
+# Auxolarity functions
+def __gradient(img: np.ndarray):
+    I_x = cv2.filter2D(img, -1, GRADIENT)
+    I_y = cv2.filter2D(img, -1, GRADIENT.T)
+    return I_x, I_y
+def is_window_valid(eigen_value: np.ndarray) -> bool:
+    lamda1 = np.max(eigen_value)
+    lamda2 = np.min(eigen_value)
+    if lamda2 <= 1 or lamda1 / lamda2 >= 100:
+        return False
+    return True
+def __eigenvalues_checking(At_mult_A: np.ndarray) -> bool:
+    """
+    1. Get the At_mult_A - a squred matrix.
+    2. Find the eigen values.
+        * Using np.linalg.eigvals, not np.linalg.eig, for getting just eigen values, without eigen vectors.
+    3. Return validation checking result (with is_window_valid() func)
+    """
+    eigen_values = np.linalg.eigvals(At_mult_A)
+    return is_window_valid(eigen_values)
 
 
 def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10,
@@ -47,7 +71,7 @@ def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10,
     
     # Optimize LK value by the Iterative Algorithm
     #   1. Calculate x, y gradiante
-    I_x, I_y = __get_directions(img1)
+    I_x, I_y = __gradient(img1)
     #   2. Calculate temporal gradient
     I_t = np.subtract(img1, img2)
     
@@ -61,15 +85,28 @@ def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10,
     max_iterations_cols = width - int(max(step_size, win_size) / 2)
     for i in range(start ,max_iterations_rows ,step_size):
         for j in range(start, max_iterations_cols, step_size):
-            x_win = I_x[i - half_window_size: i + half_window_size + 1, j - half_window_size: j + half_window_size + 1]
-            y_win = I_y[i - half_window_size: i + half_window_size + 1, j - half_window_size: j + half_window_size + 1]
-            A = np.hstack((x_win.reshape(window_pixels_num, 1), y_win.reshape(window_pixels_num, 1)))
-            if not __acceptable_eigenvalues(A):
+            """
+            For solving the:               # T   #   -1  # T  #
+                                u         ###   ###     ###   ###
+                                v   =   (#   # #   #)  #   #  ###
+                to every pixel.
+            """
+            #   3. Extract local windows
+            x_window = I_x[i - half_window_size: i + half_window_size + 1, j - half_window_size: j + half_window_size + 1]
+            y_window = I_y[i - half_window_size: i + half_window_size + 1, j - half_window_size: j + half_window_size + 1]
+            t_window = I_t[i - half_window_size: i + half_window_size + 1, j - half_window_size: j + half_window_size + 1]
+            b = (-1) * t_window.reshape(window_pixels_num, 1)
+            #   4. Compute A matrix of local windows
+            A = np.hstack((x_window.reshape(window_pixels_num, 1), y_window.reshape(window_pixels_num, 1)))
+            At_mult_A = np.dot(np.transpose(A), A)
+            #   * Eigenvalues checking
+            if not __eigenvalues_checking(At_mult_A):
                 continue
-            t_win = I_t[i - half_window_size: i + half_window_size + 1, j - half_window_size: j + half_window_size + 1]
-            b = (-1) * t_win.reshape(window_pixels_num, 1)
+            #   5. Solve the formula for the local windows to estimate optical flow (u, v)
+            At_mult_b = np.dot(np.transpose(A), b)
+            u_v = np.linalg.inv(At_mult_A) @ At_mult_b
+            #   6. Update U(x, y) and V(x, y) with the estimated optical flow (u, v)
             y_x_list.append((j, i))
-            u_v = np.dot(np.linalg.pinv(A), b)
             u_v_list.append(u_v)
     return np.array(y_x_list).reshape(-1, 2), np.array(u_v_list).reshape(-1, 2)
 
